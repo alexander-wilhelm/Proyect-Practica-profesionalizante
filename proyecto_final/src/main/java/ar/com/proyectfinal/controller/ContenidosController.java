cddf3de228e798e50e68d9bf7de2a4a6e2318c6a
maglione8 <caromaglione90@gmail.com>
2022-10-04 18:19:49 -0300

    correcion vistas

--- proyecto_final/src/main/java/ar/com/proyectfinal/controller/ContenidosController.java
+++ proyecto_final/src/main/java/ar/com/proyectfinal/controller/ContenidosController.java
@@ -17,7 +17,14 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -146,5 +153,42 @@ public class ContenidosController {
         entityService.save(contenido);
         return "redirect:/contenidos";
     }
+
+    @RequestMapping(value = "archivos", method = RequestMethod.POST)
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        String UPLOADED_FOLDER = "/temp";
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "../contenidos/index";
+    }
+
+    @RequestMapping("/u")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+
+}
+
+
+
+
 
 }
