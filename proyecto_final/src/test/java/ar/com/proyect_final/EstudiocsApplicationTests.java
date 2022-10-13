82a9fcc22e90c5ddb81980d982a0cb7490f88c1d
maglione8 <caromaglione90@gmail.com>
2022-10-13 18:48:54 -0300

    cargamos los nuevo a master

---
+++ proyecto_final/src/test/java/ar/com/proyect_final/EstudiocsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.proyect_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
